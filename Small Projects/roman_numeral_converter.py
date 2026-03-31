f9b4de5adf97193df2fd09b342a3d88c52ce98e3
JB-Portfolio-Projects <jfb7411@gmail.com>
2026-03-31 10:08:27 -0400

    produced code, tested. Had some issues but figured out I was missing come commas, and num -+ value was incorrect it should be num -= value. Tested and prodcues results. This will be version 1.0, I'll refine it later.

---
+++ Small Projects/roman_numeral_converter.py
@@ -0,0 +1,18 @@
+# Define number values to the presepctive roman numeral
+def to_roman(num):
+    values = [
+        (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+        (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+        (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+    ] 
+#input and get result
+    result = ""
+    for value, symbol in values:
+        while num >= value:
+            result += symbol
+            num -= value
+
+    return result
+
+year = int(input("Enter a year: "))
+print("Roman Numeral:", to_roman(year))
