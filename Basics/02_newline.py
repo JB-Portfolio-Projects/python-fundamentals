70fc8da65da51a1d2c2b8a768808675fe2adef0e
JB-Portfolio-Projects <jfb7411@gmail.com>
2026-04-05 10:44:23 -0400

    added keyword arguments

--- Basics/02_newline.py
+++ Basics/02_newline.py
@@ -5,3 +5,7 @@ print("Down came the rain\nand washed the spider out")
 
 #using multiple arguments within the string
 print("The itsy bitsy spider", "climbed up", "the waterspout")
+
+#used keyword arguments
+print("My name is", "Python.", end=" ")
+print("Monty Python")
