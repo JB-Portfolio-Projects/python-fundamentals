b9e28b302d7b6dbd108bea8b7ba2da6bafd6aca8
JB-Portfolio-Projects <jfb7411@gmail.com>
2026-04-05 13:21:29 -0400

    practiced different uses of operators

---
+++ Basics/02-4_operators.py
@@ -0,0 +1,16 @@
+print(14 % 4)
+print(-4 - 4)
+print(-4 + 4)
+print(4. - 8)
+print(-1.1)
+print(+2)
+
+#Add multiple operators
+print(2 + 3 * 5)
+print(9 % 6 % 2)
+print(2 ** 2 ** 3)
+print(2 * 3 % 5)
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+print((2 ** 4), (2 * 4.), (2 * 4))
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
