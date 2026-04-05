ce14d386d5f0cd10ef404b5a079d1200789b4238
JB-Portfolio-Projects <jfb7411@gmail.com>
2026-04-05 14:18:15 -0400

    more excercises with variables

---
+++ Basics/02-5_variables.py
@@ -0,0 +1,35 @@
+#use variables declarations
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+
+
+var = "3.8.5"
+print("Python version: " + var)
+
+#assign new value to existing variable
+var = 1
+print(var)
+var = var + 1
+print(var)
+
+var = 100
+var = 200 + 300
+print(var)
+
+#math problem
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+#excerise word problem
+john = 3
+mary = 5
+adam = 6
+print(john, mary, adam)
+
+total_apples = john + mary + adam
+print(total_apples)
