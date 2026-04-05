11b14c0063dc2df8640ac681ae0cd10a5e8dc6f2
JB-Portfolio-Projects <jfb7411@gmail.com>
2026-04-05 14:54:59 -0400

    practice inputs with floats, repliaction with strings.

---
+++ Basics/02-6_inputs.py
@@ -0,0 +1,21 @@
+#create input for user to enter value
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+hypo = (leg_a**2 + leg_b**2) ** .5
+print("Hypotenuse length is", hypo)
+
+#simplify
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+print("Hypotenuse length is", (leg_a**2 + leg_b**2) ** .5)
+
+#first and last name
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
+
+#replication
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
