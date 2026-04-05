b0b1faffdd697fc20713752789ea54168f0919a6
JB-Portfolio-Projects <jfb7411@gmail.com>
2026-04-05 10:36:16 -0400

    added multiple arguments within the string and print.

--- Basics/02_newline.py
+++ Basics/02_newline.py
@@ -2,3 +2,6 @@
 print("The itsy bitsy spider\nclimbed up the water spout")
 print()
 print("Down came the rain\nand washed the spider out")
+
+#using multiple arguments within the string
+print("The itsy bitsy spider", "climbed up", "the waterspout")
