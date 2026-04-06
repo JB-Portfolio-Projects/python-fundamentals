c6487768a00c9a3e7ca2e93f484e1c2dd1960596
JB-Portfolio-Projects <jfb7411@gmail.com>
2026-04-06 08:54:06 -0400

    Weather App v1.0 - Added input - Added way to store data - output response

---
+++ Small Projects/weather-app_api.py
@@ -0,0 +1,6 @@
+#v1.0, add user input, store data, and output response
+print("=== Weather App v1.0 ===")
+
+city = input("Enter a city: ")
+print(f"fetching weather for: {city}")
+print("Weather data coming soon...")
